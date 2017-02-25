871a4a2e5aaaaf3c3bacdc76c428fe18d4227f22
Aegen <kyuzo101@gmail.com>
2017-02-25 12:08:21 -0600

    Navigation Bar added with Menu Items, clicking them does nothing

--- app/src/main/java/com/example/aegis/budgpal/MainActivity.java
+++ app/src/main/java/com/example/aegis/budgpal/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.aegis.budgpal;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout NavDrawer;
+    private ListView NavDrawerList;
+    private String[] NavDrawerItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NavDrawer      = (DrawerLayout)findViewById(R.id.navDrawer);
+        NavDrawerList  = (ListView)findViewById(R.id.navDrawerList);
+        NavDrawerItems = getResources().getStringArray(R.array.navListItems);
+
+        NavDrawerList.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, NavDrawerItems));
     }
 }
