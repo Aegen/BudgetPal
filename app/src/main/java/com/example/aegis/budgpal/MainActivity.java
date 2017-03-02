26c12815c3cb571d4160201abf4e1374a99ddf59
Aegen <kyuzo101@gmail.com>
2017-03-02 15:49:07 -0600

    Nav Drawer now Navs, removed buttons

--- app/src/main/java/com/example/aegis/budgpal/MainActivity.java
+++ app/src/main/java/com/example/aegis/budgpal/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.aegis.budgpal;
 
+import android.content.Intent;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,6 +10,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,15 +26,35 @@ public class MainActivity extends AppCompatActivity {
         NavDrawer      = (DrawerLayout)findViewById(R.id.navDrawer);
         NavDrawerList  = (ListView)findViewById(R.id.navDrawerList);
         NavDrawerItems = getResources().getStringArray(R.array.navListItems);
-
         NavDrawerList.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, NavDrawerItems));
 
         NavDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(getApplicationContext(), parent.getItemAtPosition(position).toString(),
-                        Toast.LENGTH_SHORT).show();
+                /*Toast.makeText(getApplicationContext(), parent.getItemAtPosition(position).toString(),
+                        Toast.LENGTH_SHORT).show();*/
                 NavDrawer.closeDrawer(Gravity.LEFT);
+                Intent tempIntent;
+                switch (parent.getItemAtPosition(position).toString()){
+                    case "Change Budget":
+                        tempIntent = new Intent(MainActivity.this, Set_Budget.class);
+                        break;
+                    case "Add Expense":
+                        tempIntent = new Intent(MainActivity.this, Add_Expenses.class);
+                        break;
+                    case "Add Event":
+                        tempIntent = new Intent(MainActivity.this, Add_Event.class);
+                        break;
+                    case "View Expenses":
+                        tempIntent = new Intent(MainActivity.this, View_History.class);
+                        break;
+                    default:
+                        Toast.makeText(getApplicationContext(), "Not Implemented", Toast.LENGTH_SHORT).show();
+                        Log.e("Invalid Selection", parent.getItemAtPosition(position).toString() + " failed to match with an activity.");
+                        return;
+                }
+
+                startActivity(tempIntent);
             }
         });
     }
