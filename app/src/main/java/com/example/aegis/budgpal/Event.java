31e9b2a171421f768701e6d0d82e6746842c8b3a
Aegen <kyuzo101@gmail.com>
2017-03-01 18:14:10 -0600

    Added getters and setters

---
+++ app/src/main/java/com/example/aegis/budgpal/Event.java
@@ -0,0 +1,78 @@
+package com.example.aegis.budgpal;
+
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+/**
+ * Created by Harrison on 2/25/2017.
+ */
+
+public class Event {
+
+
+    private int eventID;
+    private int userID;
+    private Date lastModified;
+    private Date startDate;
+    private Date endDate;
+    private String description;
+    private boolean deleted;
+
+
+    public int getEventID() {
+        return eventID;
+    }
+
+    public void setEventID(int eventID) {
+        this.eventID = eventID;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public Date getLastModified() {
+        return lastModified;
+    }
+
+    public void setLastModified(Date lastModified) {
+        this.lastModified = lastModified;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        this.deleted = deleted;
+    }
+
+}
