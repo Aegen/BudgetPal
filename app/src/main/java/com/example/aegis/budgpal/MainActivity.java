94e10164e69e8d0c07ef7831ecf52bac299f0c1b
Harrison <harrison.hruby@huskers.unl.edu>
2017-03-01 20:11:51 -0600

    added further functionality

--- app/src/main/java/com/example/aegis/budgpal/MainActivity.java
+++ app/src/main/java/com/example/aegis/budgpal/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.aegis.budgpal;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteCursorDriver;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteQuery;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -21,22 +28,27 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i("first", "first");
+        UserDatabaseHandler a = new UserDatabaseHandler(getApplicationContext(), "database", null, 1);
+        SQLiteDatabase db = a.getWritableDatabase(); //Create Database object
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         boolean dbExists = checkForDatabase();
+        db.execSQL("INSERT INTO User (Username, HashedPassword, LastModified, Deleted) VALUES ('harrison', 'password', '1996-01-01 12:00:00', 0);"); //Load item into db
+        Cursor curse = db.query("User", new String[]{"UserID", "Username", "HashedPassword", "LastModified", "Deleted"},null, null, null, null, null); //get Cursor for objects in db
 
         NavDrawer      = (DrawerLayout)findViewById(R.id.navDrawer);
         NavDrawerList  = (ListView)findViewById(R.id.navDrawerList);
         NavDrawerItems = getResources().getStringArray(R.array.navListItems);
-
+        //Toast.makeText(getApplicationContext(), Integer.toString(curse.getColumnCount()), Toast.LENGTH_LONG).show(); //print number of columns
+        //runToast.makeText(getApplicationContext(), curse.getString(1), Toast.LENGTH_LONG).show();
         NavDrawerList.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, NavDrawerItems));
 
         NavDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Toast.makeText(getApplicationContext(), parent.getItemAtPosition(position).toString(),
-                        Toast.LENGTH_SHORT).show();
+                Toast.LENGTH_SHORT).show();
                 NavDrawer.closeDrawer(Gravity.LEFT);
             }
         });
