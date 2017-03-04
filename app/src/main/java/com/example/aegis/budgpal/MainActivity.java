c28e6ba144c33c03009fc160ab6fd4ed300b572c
Aegen <kyuzo101@gmail.com>
2017-03-03 18:17:12 -0600

    Change to SwitchManager

--- app/src/main/java/com/example/aegis/budgpal/MainActivity.java
+++ app/src/main/java/com/example/aegis/budgpal/MainActivity.java
@@ -34,27 +34,11 @@ public class MainActivity extends AppCompatActivity {
                 /*Toast.makeText(getApplicationContext(), parent.getItemAtPosition(position).toString(),
                         Toast.LENGTH_SHORT).show();*/
                 NavDrawer.closeDrawer(Gravity.LEFT);
-                Intent tempIntent;
-                switch (parent.getItemAtPosition(position).toString()){
-                    case "Change Budget":
-                        tempIntent = new Intent(MainActivity.this, SetBudget.class);
-                        break;
-                    case "Add Expense":
-                        tempIntent = new Intent(MainActivity.this, AddExpenses.class);
-                        break;
-                    case "Add Event":
-                        tempIntent = new Intent(MainActivity.this, AddEvent.class);
-                        break;
-                    case "View Expenses":
-                        tempIntent = new Intent(MainActivity.this, ViewHistory.class);
-                        break;
-                    default:
-                        Toast.makeText(getApplicationContext(), "Not Implemented", Toast.LENGTH_SHORT).show();
-                        Log.e("Invalid Selection", parent.getItemAtPosition(position).toString() + " failed to match with an activity.");
-                        return;
-                }
+                Intent tempIntent = SwitchManager.SwitchActiviy(MainActivity.this, parent.getItemAtPosition(position).toString());
 
-                startActivity(tempIntent);
+                if(tempIntent != null){
+                    startActivity(tempIntent);
+                }
             }
         });
     }
