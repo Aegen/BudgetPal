fbda1a35f0614ee8f52329f663b020f16b0a8ca6
Kyle <kylecrowderiscool@gmail.com>
2017-03-16 15:03:34 -0500

    Added 'Update' and 'Delete' to ExpenseDetails but they aren't working quite yet.

--- app/src/main/java/com/example/aegis/budgpal/ExpenseDetailsActivity.java
+++ app/src/main/java/com/example/aegis/budgpal/ExpenseDetailsActivity.java
@@ -2,7 +2,10 @@ package com.example.aegis.budgpal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class ExpenseDetailsActivity extends AppCompatActivity {
 
@@ -10,6 +13,9 @@ public class ExpenseDetailsActivity extends AppCompatActivity {
     private EditText DescriptionField;
     private EditText CreatedByField;
     private EditText CreatedOnField;
+
+    private Button UpdateButton;
+    private Button DeleteButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +26,58 @@ public class ExpenseDetailsActivity extends AppCompatActivity {
         DescriptionField = (EditText)findViewById(R.id.descriptionField);
         CreatedByField = (EditText)findViewById(R.id.CreatedByField);
         CreatedOnField = (EditText)findViewById(R.id.CreatedOnField);
+        UpdateButton = (Button)findViewById(R.id.expenseDetailsUpdateButton);
+        DeleteButton = (Button)findViewById(R.id.expenseDetailsDeleteButton);
+        UpdateButton.setEnabled(false);
 
         AmountField.setText(getIntent().getStringExtra("Amount"));
         DescriptionField.setText(getIntent().getStringExtra("Description"));
         CreatedByField.setText(Long.toString(getIntent().getLongExtra("User", -1)));
         CreatedOnField.setText(getIntent().getStringExtra("LastModified"));
+
+        DeleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Expense ex = StatUtils.GetExpense(getApplicationContext(), getIntent().getLongExtra("ExpenseID", new Long(-1)));
+                ex.setDeleted(true);
+                ex.pushToDatabase();
+
+                setResult(RESULT_OK);
+//                EventDetailsActivity.super.onBackPressed();
+                finish();
+            }
+        });
+
+        UpdateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Expense ex = StatUtils. GetExpense(getApplicationContext(), getIntent().getLongExtra("ExpenseID", new Long(-1)));
+
+                if(ex.getUserID() == -1){
+                    Toast.makeText(getApplicationContext(), "No expense found", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                ex.setAmount(Float.parseFloat(AmountField.getText().toString()));
+                ex.setDescription(DescriptionField.getText().toString());
+                ex.setLastModified(StatUtils.GetCurrentDate());
+                ex.pushToDatabase();
+
+                setResult(RESULT_OK);
+                ExpenseDetailsActivity.super.onBackPressed();
+                finish();
+
+            }
+        });
+
+        if(getIntent().getLongExtra("ExpenseID", new Long(-1)) != -1){
+            Expense house = StatUtils.GetExpense(getApplicationContext(), getIntent().getLongExtra("ExpenseID", new Long(-1)));
+            AmountField.setText(house.getAmount() + "");
+            DescriptionField.setText(house.getDescription());
+            CreatedByField.setText(StatUtils.GetUser(getApplicationContext(), house.getUserID()).getUsername());
+            CreatedOnField.setText(house.getLastModified());
+
+            UpdateButton.setEnabled(true);
+
+        }
     }
 }
