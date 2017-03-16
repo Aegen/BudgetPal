55376e34a6c845e6053b3de6dd9787f296000f2d
Aegen <kyuzo101@gmail.com>
2017-03-16 12:58:30 -0500

    Can now edit and delete events

--- app/src/main/java/com/example/aegis/budgpal/EventDetailsActivity.java
+++ app/src/main/java/com/example/aegis/budgpal/EventDetailsActivity.java
@@ -1,9 +1,13 @@
 package com.example.aegis.budgpal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class EventDetailsActivity extends AppCompatActivity {
 
@@ -11,6 +15,9 @@ public class EventDetailsActivity extends AppCompatActivity {
     private EditText DateField;
     private EditText CreatedByField;
     private EditText CreatedOnField;
+    private Button UpdateButton;
+    private Button DeleteButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +28,60 @@ public class EventDetailsActivity extends AppCompatActivity {
         DescriptionField = (EditText)findViewById(R.id.DescriptionField);
         CreatedByField = (EditText)findViewById(R.id.CreatedByField);
         CreatedOnField = (EditText)findViewById(R.id.CreatedOnField);
+        UpdateButton = (Button)findViewById(R.id.eventDetailsUpdateButton);
+        DeleteButton = (Button)findViewById(R.id.eventDetailsDeleteButton);
+        UpdateButton.setEnabled(false);
 
-        DescriptionField.setText(getIntent().getStringExtra("Description"));
-        DateField.setText(getIntent().getStringExtra("Date"));
-        CreatedByField.setText(StatUtils.GetUser(getApplicationContext(), getIntent().getLongExtra("User", -1)).getUsername());
-        CreatedOnField.setText(getIntent().getStringExtra("LastModified"));
+        DeleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Event ev = StatUtils.GetEvent(getApplicationContext(), getIntent().getLongExtra("EventID", new Long(-1)));
+                ev.setDeleted(true);
+                ev.pushToDatabase();
+
+                setResult(RESULT_OK);
+//                EventDetailsActivity.super.onBackPressed();
+                finish();
+            }
+        });
+
+        UpdateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Event ev = StatUtils.GetEvent(getApplicationContext(), getIntent().getLongExtra("EventID", new Long(-1)));
+
+                if(ev.getUserID() == -1){
+                    Toast.makeText(getApplicationContext(), "No event found", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                ev.setDescription(DescriptionField.getText().toString());
+                if(StatUtils.IsValidDate(DateField.getText().toString())){
+                    ev.setStartDate(DateField.getText().toString());
+                    ev.setEndDate(DateField.getText().toString());
+                }else{
+                    Toast.makeText(getApplicationContext(), "Invalid date", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                ev.setLastModified(StatUtils.GetCurrentDate());
+
+                ev.pushToDatabase();
+
+                setResult(RESULT_OK);
+                EventDetailsActivity.super.onBackPressed();
+                finish();
+
+            }
+        });
+
+        if(getIntent().getLongExtra("EventID", new Long(-1)) != -1){
+            Event house = StatUtils.GetEvent(getApplicationContext(), getIntent().getLongExtra("EventID", new Long(-1)));
+            DescriptionField.setText(house.getDescription());
+            DateField.setText(house.getStartDate());
+            CreatedByField.setText(StatUtils.GetUser(getApplicationContext(), house.getUserID()).getUsername());
+            CreatedOnField.setText(house.getLastModified());
+
+            UpdateButton.setEnabled(true);
+
+        }
     }
 }
