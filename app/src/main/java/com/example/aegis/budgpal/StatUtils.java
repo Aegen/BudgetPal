833af4a2d226ee91bb1c5c1eef317c5ffee674d8
Aegen <kyuzo101@gmail.com>
2017-03-17 00:54:37 -0500

    Rebased with master

--- app/src/main/java/com/example/aegis/budgpal/StatUtils.java
+++ app/src/main/java/com/example/aegis/budgpal/StatUtils.java
@@ -518,7 +518,7 @@ public class StatUtils {
         SQLiteDatabase db = GetDatabase(context);
         ArrayList<Budget> output = new ArrayList<>();
 
-        Cursor curs = db.rawQuery("SELECT * FROM Budget WHERE UserID = " + UserID + " ORDER BY StartDate DESC", null);
+        Cursor curs = db.rawQuery("SELECT * FROM Budget WHERE UserID = " + UserID + " ORDER BY BudgetID DESC", null);
 
         if(curs.getCount() > 0) {
             curs.moveToFirst();
